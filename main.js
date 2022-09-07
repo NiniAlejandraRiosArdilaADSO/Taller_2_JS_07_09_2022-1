83ad3932a8ce52404999588d7fce059fdbca0999
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-09-07 12:00:53 -0500

    ejercicio 6

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 addEventListener("DOMContentLoaded", ()=>{
-    // 5. Escribir un programa que calcule la longitud y el área de una circunferencia: Radio = 4
-    // Longitud de la circunferencia = 2 * PI * radio
-    // Área de la circunferencia = PI * radio˄2
-
-
-    let radio = Number(prompt("Ingrese el radio de la circunferencia", 5));
-
-
-    console.group(`%cFormula ${2} * ${Math.PI} * ${radio}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-
-        console.log(`Longitud de la circunferencia : "${2 * Math.PI * radio}"`);
-    console.groupEnd();
-
-    console.group(`%cFormula ${Math.PI} * ${Math.pow(radio, 2)}`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-        console.log(`Área de la circunferencia : "${Math.PI * Math.pow(radio, 2)}"`);
-    console.groupEnd();
-})
+    //  Escribir un programa que calcule la velocidad de un proyectil que recorre 2 Km en 5 minutos. Expresar el resultado en metros/segundo: ")
+  
+  
+    let distancia = Number(prompt("ingrese el vaor de la distancia ", 5));
+  
+    let tiempo = Number(prompt("ingrese el tiempo ", 10));
+  
+  
+    console.group(`%cFormula (${5} * ${1000}) / (${tiempo} * ${60})`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
+  
+        console.log(`la velocidad del proyectil : "${5 * 1000 / tiempo * 60}"`);
+    
+  
+  })
+  
+  
