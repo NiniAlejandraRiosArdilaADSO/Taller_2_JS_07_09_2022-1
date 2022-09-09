607a56d1151540c62b8941fc8d1a2b27534d3ab0
NiniAlejandraRiosArdilaADSO <ninialejandra@gmail.com>
2022-09-08 20:48:01 -0500

    ejercicio 7

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 addEventListener("DOMContentLoaded", ()=>{
-    //  Escribir un programa que calcule la velocidad de un proyectil que recorre 2 Km en 5 minutos. Expresar el resultado en metros/segundo: ")
+    //   Escribir un programa que calcule el volumen de una esfera: ")
   
   
-    let distancia = Number(prompt("ingrese el vaor de la distancia ", 5));
-  
-    let tiempo = Number(prompt("ingrese el tiempo ", 10));
+    let radio = Number(prompt("ingrese el radio de la esfera ", 3));
   
   
-    console.group(`%cFormula (${5} * ${1000}) / (${tiempo} * ${60})`, `background: rebeccapurple; color: antiquewhite; font-family: cursive; padding: 2px; border-radius: 1px solid #925fc4; border-radius: 5px;`);
-  
-        console.log(`la velocidad del proyectil : "${5 * 1000 / tiempo * 60}"`);
+    console.group();
+        alert(`calcule el volumen de la esfera : "${(4/3) * Math.PI * Math.pow(radio,3)}`)
+    console.groupEnd();
     
-  
   })
   
   
