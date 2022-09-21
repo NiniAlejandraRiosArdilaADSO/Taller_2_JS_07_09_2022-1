6265ca9a0dced3e1755e3c77af06c9c4437f5cf3
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-09-21 11:44:17 -0500

    EJERCICIO 1/GUIA2

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 addEventListener(`DOMContentLoaded`, ()=>{
-    //   Un vendedor recibe un sueldo base más el 10% de comisión sobre sus ventas. Si en un mes cualquiera hace tres ventas por valores:
+    //   Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución, por favor utilizar funciones matemáticas de Python para ello 
 
-    let v1 =Number(prompt(`VALOR DE LA VENTA 1 :  `, 4000));
-    let v2 =Number(prompt(`VALOR DE LA VENTA 2 :  `, 3000));
-    let v3 =Number(prompt(`VALOR DE LA VENTA 3 :  `, 2000));
-    let sueldo =Number(prompt(`INGRESA TU SUELDO BASE  :  `, 50000));
-    let comision = (v1 + v2 + v3 ) * .10
-    let total =(sueldo + comision)
-    
-    
-    console.log (`SU SUELDO BASE ES  : ${sueldo} `)
-    console.log (`SU COMISION DEL MES ES  : ${comision}`)
-    console.log (`SU SUELDO TOTAL DEL MES ES  : ${total}`)
+    let x1 =Number(prompt(`x1 :  `, 4));
+    let x2 =Number(prompt(`x2 :  `, 5));
+    let y1 =Number(prompt(`y1 :  `, 6));
+    let y2 =Number(prompt(`y2 :  `, 7));
+    let distancia = ((x2 - x1) * (x2-x1) + (y2-y1) * (y2-y1))
+ 
+    console.log (`VALOR DE DISTANCIA  : ${distancia} `)
 
 })
