6d2a77e1665ff96996be58c2ee607c6c3d601453
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-09-22 08:07:14 -0500

    EJERCICIO 2/GUIA2

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 addEventListener(`DOMContentLoaded`, ()=>{
-    //   Se requiere calcular la distancia entre dos puntos, realizar el código que permita hallar la solución, por favor utilizar funciones matemáticas de Python para ello 
+    //   Pablo compro 5 productos los dos primeros con un 5% de descuento y los 2 últimos con un 2% de  descuento, realice un algoritmo que indique cuanto fue el valor a pagar por cada producto y el total  de la compra
 
-    let x1 =Number(prompt(`x1 :  `, 4));
-    let x2 =Number(prompt(`x2 :  `, 5));
-    let y1 =Number(prompt(`y1 :  `, 6));
-    let y2 =Number(prompt(`y2 :  `, 7));
-    let distancia = ((x2 - x1) * (x2-x1) + (y2-y1) * (y2-y1))
- 
-    console.log (`VALOR DE DISTANCIA  : ${distancia} `)
+    let cp =Number(prompt(`VALOR DE LA COMPRA 1 :  `, 5000));
+    let c2 =Number(prompt(`VALOR DE LA COMPRA 2 : `, 5200));
+    let c3 =Number(prompt(`VALOR DE LA COMPRA 3 :`, 2500));
+    let c4 =Number(prompt(`VALOR DE LA COMPRA 4 :  `, 200));
+    let x5 =Number(prompt(`VALOR DE LA COMPRA 5 :` , 600 ))
+    let des = (cp+c2) * 0.5
+    let des2 = (c3+c4) * 0.2
+    let suma = (des+des2+X5)
+
+    console.log(`COMPPRAS  : ${cp} ${c2} ${c3} ${c4} ${x5}`)
+    console.log(`TOTAL DE COMPRAS CON EL 5 % DE DESCUENTO : $ ${des}`)
+    console.log(`TOTAL DE COMPRAS CON EL 2 % DE DESCUENTO : $ ${des2}`)
+    console.log(`PRODUCTO SIN DECUENTO  : $ ${x5}`)
+    console.log(`TOTAL : ${suma}`)
 
 })
