d17d70f54f55b41b76974076e6c041db993573b2
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-09-22 08:45:56 -0500

    EJECICIO 6/GUIA2

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 addEventListener(`DOMContentLoaded`, ()=>{
-    //   En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para hacer el bizcocho es 
+    //   Un camión transporta 25 cajas de repuestos de carros. Si cada caja pesa 748 Kg ¿ Cuántos Kg transporta ? 
 
-    let harina=100
-    let kakao=10
-    let maskakao=20
+    let caja=25
+    let peso=748
+    let c1=1*748
+    let suma=caja*peso
 
-    let mult=harina*maskakao
-    let div=mult/kakao
-
-    console.log(`LA CANTIDAD DE GRAMOS DE HARINNA PARA HACER EL BIZCOCHO ES :  ${div}`)
+    console.log(`TRANSPORTA : KG ${suma}`)
 
 
 })
