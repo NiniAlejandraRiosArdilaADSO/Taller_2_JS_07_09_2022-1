9c68912834cf4c71aba9747a7a7790ed6fa92ffe
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-09-22 11:06:20 -0500

    EJERCICIO 10/GUIA2

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
 addEventListener(`DOMContentLoaded`, ()=>{
-    //   Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo, pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código que representen el algoritmo para solucionar este problema.Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula sostenida y la edad del entrevistado.
+    //   Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase invertida.
+
                                                     
-    let n = (prompt(`ESCRIBE TU NOMBRE  : `))
-    let a = Number(prompt(`ESCRIBE EL AÑO ACTUAL : `,2022)) 
-    let c = Number(prompt(`ESCRIBE TU FECHA DE NACIMIENTO  : `,2005))
-    
-    let rta = a-c
-    
-    console.log(`VIENVENID@ :  ${a}`)
-    console.log(`VIENVENID@ :  ${rta}`)
+    let frase = (prompt(`ESCRIBE SU FRASE  : `))
 
-
+    console.log(`FRASE :  ${frase[::-1]}`)
 
 })
