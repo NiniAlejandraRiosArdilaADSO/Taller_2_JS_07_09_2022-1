45a9b4a17a64f43ba865ba67a8b9376f51f01710
NiniAlejandraRiosArdilaADSO <ninialejandra0424@gmail.com>
2022-10-13 08:19:24 -0500

    Punto 1

---
+++ Taller_2_JS_07_09_2022-1/main.js
@@ -0,0 +1,9 @@
+addEventListener(`DOMContentLoaded`, ()=>{
+    //   Ingrese una frase que lo identifique como programador de Software y luego mostar esta frase invertida.
+
+                                                    
+    let frase = (prompt(`ESCRIBE SU FRASE  : `))
+
+    console.log(`FRASE :  ${frase[::-1]}`)
+
+})
